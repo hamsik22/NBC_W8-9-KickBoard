49218fd1cc854dbee8f54fa5136c4c9d7d8f2604
HamSik <payback9488@gmail.com>
2024-12-13 16:25:57 +0900

    [proj]: Create Project
    - CodeBase Setting
    - SnapKit
    - Alamofire

---
+++ Quick-Kick/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Quick-Kick
+//
+//  Created by 황석현 on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController DidLoad")
+    }
+}
+
