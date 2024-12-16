c1d7c992d6c0e3d7975eea463c4355c04ca91080
Crois0509 <crois0509@naver.com>
2024-12-16 16:50:10 +0900

    mod: 탭 바 아이템 UI 수정

--- Quick-Kick/TabBar/TabBarItem.swift
+++ Quick-Kick/TabBar/TabBarItem.swift
@@ -15,13 +15,13 @@ final class TabBarItem: UICollectionViewCell {
     private let tabTitle: [String] = ["킥보드 찾기", "킥보드 등록", "마이 페이지"]
     private let tabImage: [UIImage] = []
     
-    private let tabButton =  UIButton()
+    private let tabLabel =  UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.backgroundColor = .clear
-        self.addSubview(self.tabButton)
+        self.addSubview(self.tabLabel)
         
         configUI()
     }
@@ -30,7 +30,7 @@ final class TabBarItem: UICollectionViewCell {
         super.init(coder: coder)
         
         self.backgroundColor = .clear
-        self.addSubview(self.tabButton)
+        self.addSubview(self.tabLabel)
         
         configUI()
     }
@@ -41,35 +41,29 @@ final class TabBarItem: UICollectionViewCell {
     }
     
     private func setupButton() {
-        self.tabButton.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        self.tabButton.setTitleColor(UIColor.PersonalDark.darker, for: .normal)
-        self.tabButton.setTitleColor(UIColor.PersonalNomal.nomal, for: .selected)
-        self.tabButton.backgroundColor = .clear
-        self.tabButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-    }
-    
-    @objc private func buttonTapped() {
-        // VC 변경 메소드
-        print(#function)
+        self.tabLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        self.tabLabel.textColor = UIColor.PersonalDark.darker
+        self.tabLabel.numberOfLines = 1
+        self.tabLabel.textAlignment = .center
+        self.tabLabel.backgroundColor = .clear
     }
     
     private func setupLayout() {
-        self.tabButton.snp.makeConstraints {
+        self.tabLabel.snp.makeConstraints {
             $0.top.bottom.leading.trailing.equalToSuperview()
         }
     }
     
     func setupButtonConfig(_ row: Int) {
-        self.tabButton.setTitle(self.tabTitle[row], for: .normal)
-        self.tabButton.setTitle(self.tabTitle[row], for: .selected)
-        self.tabButton.tag = row
+        self.tabLabel.text = self.tabTitle[row]
+        self.tabLabel.tag = row
     }
     
     func selectedTab(_ isSelected: Bool) {
         guard isSelected else { return }
         
-        self.tabButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        self.tabButton.isSelected = true
-        self.tabButton.layoutIfNeeded()
+        self.tabLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        self.tabLabel.textColor = UIColor.PersonalNomal.nomal
+        self.tabLabel.layoutIfNeeded()
     }
 }
