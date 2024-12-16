318ac4ec8652ff30846531659a8909f3389408ea
zzangmmz <leemj036@gmail.com>
2024-12-16 13:31:12 +0900

    file: SearchKickboardMapView 파일 생성

---
+++ Quick-Kick/SearchKickboard/View/SearchKickboardMapView.swift
@@ -0,0 +1,12 @@
+//
+//  SearchKickboardMapView.swift
+//  Quick-Kick
+//
+//  Created by 이명지 on 12/16/24.
+//
+import UIKit
+import MapKit
+
+final class SearchKickboardMapView: MKMapView {
+    
+}
