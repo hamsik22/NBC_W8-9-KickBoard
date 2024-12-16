a43253f59dc50010a239da33d2f7dac4bf62b014
emilyj4482 <emilyj4482@gmail.com>
2024-12-16 13:33:45 +0900

    add: 임시 현위치 강남역으로 맵뷰 그리기

--- Quick-Kick/AddKickboard/MapView.swift
+++ Quick-Kick/AddKickboard/MapView.swift
@@ -8,11 +8,23 @@
 import MapKit
 
 class MapView: MKMapView {
+    
+    // temporary current location
+    private let latitude: CLLocationDegrees = 37.497952
+    private let longitude: CLLocationDegrees = 127.027619
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setup()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        centerCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        setCameraZoomRange(MKMapView.CameraZoomRange(minCenterCoordinateDistance: 200, maxCenterCoordinateDistance: 2000), animated: true)
     }
 }
