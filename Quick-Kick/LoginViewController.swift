f81460fa12c10209a28e19bcf41049a3ccb05fb5
HwangSukHyeon <payback9488@gmail.com>
2024-12-16 14:03:00 +0900

    file: 로그인화면 뷰컨트롤러 추가 & 주석작성

---
+++ Quick-Kick/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Quick-Kick
+//
+//  Created by 황석현 on 12/16/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // TODO: UI 선언
+    // appLogoImageView: UIImageView
+    // loginFieldStackView: UIStackView
+    // authOptionStackView: UIStackView
+    // loginButtonStackView: UIStackView
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: UI 세팅하기
+    }
+    
+    // TODO: UI 세팅 함수
+    private func setupUI() { }
+}
