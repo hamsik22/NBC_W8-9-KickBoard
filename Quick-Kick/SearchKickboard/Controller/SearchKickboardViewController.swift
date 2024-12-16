f53098243b5518147411dca48e85be18a62000d2
zzangmmz <leemj036@gmail.com>
2024-12-16 13:25:26 +0900

    file: SearchKickboardViewController 파일 생성

---
+++ Quick-Kick/SearchKickboard/Controller/SearchKickboardViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SearchKickboardViewController.swift
+//  Quick-Kick
+//
+//  Created by 이명지 on 12/16/24.
+//
+import UIKit
+
+final class SearchKickboardViewController: UIViewController {
+    
+}
