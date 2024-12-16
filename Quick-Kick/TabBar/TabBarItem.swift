1605eaabd7d6012319092d770a0db09601709411
Crois0509 <crois0509@naver.com>
2024-12-16 15:34:52 +0900

    add: 탭 바 아이템 설정 추가

--- Quick-Kick/TabBar/TabBarItem.swift
+++ Quick-Kick/TabBar/TabBarItem.swift
@@ -10,4 +10,49 @@ import SnapKit
 
 final class TabBarItem: UICollectionViewCell {
     
+    private let tabTitle: [String] = ["킥보드 찾기", "킥보드 등록", "마이 페이지"]
+    private let tabImage: [UIImage] = []
+    
+    private let tabButton =  UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+        self.addSubview(self.tabButton)
+        
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.backgroundColor = .clear
+        self.addSubview(self.tabButton)
+        
+        configUI()
+    }
+    
+    private func configUI() {
+        setupButton()
+        setupLayout()
+    }
+    
+    private func setupButton() {
+        self.tabButton.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        self.tabButton.setTitleColor(UIColor.PersonalDark.darker, for: .normal)
+        self.tabButton.setTitleColor(UIColor.PersonalNomal.nomal, for: .selected)
+        self.tabButton.backgroundColor = .clear
+    }
+    
+    private func setupLayout() {
+        self.tabButton.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    func setupButtonConfig(_ row: Int) {
+        self.tabButton.setTitle(self.tabTitle[row], for: .normal)
+        self.tabButton.setTitle(self.tabTitle[row], for: .selected)
+    }
 }
