e70ad9bf8798392865a1f7543a94fbf9199aa8f5
Crois0509 <crois0509@naver.com>
2024-12-16 16:49:38 +0900

    add: 탭바 델리게이트 생성

---
+++ Quick-Kick/TabBar/TabBarDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarDelegate.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/16/24.
+//
+
+import UIKit
+
+protocol TabBarDelegate: AnyObject {
+    
+    func changeVC(_ index: Int)
+}
