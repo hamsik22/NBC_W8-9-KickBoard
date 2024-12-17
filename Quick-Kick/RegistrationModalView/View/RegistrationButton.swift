a725dce46c0b1091cff2498aa1f36fc3f1065ce8
Crois0509 <crois0509@naver.com>
2024-12-17 17:56:43 +0900

    feat: 모달 버튼 옵션 수정

--- Quick-Kick/RegistrationModalView/View/RegistrationButton.swift
+++ Quick-Kick/RegistrationModalView/View/RegistrationButton.swift
@@ -33,20 +33,33 @@ final class RegistrationButton: UIView {
     private func setupButton() {
         self.addButton.setTitle("등록하기", for: .normal)
         self.addButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-        self.addButton.backgroundColor = .PersonalNomal.nomal
+        self.addButton.backgroundColor = .PersonalLight.light
         self.addButton.layer.cornerRadius = 30
-        self.addButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.addButton.isEnabled = false
         
         self.addSubview(self.addButton)
     }
     
     @objc private func buttonTapped() {
-        
+        // CoreData에 데이터 전달
+        print(#function)
     }
     
     private func setupLayout() {
         self.addButton.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+    }
+    
+    func activateButton(_ isActivate: Bool) {
+        switch isActivate {
+        case true:
+            self.addButton.isEnabled = true
+            self.addButton.backgroundColor = .PersonalNomal.nomal
+            self.addButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        case false:
+            self.addButton.isEnabled = false
+            self.addButton.backgroundColor = .PersonalLight.light
         }
     }
 }
