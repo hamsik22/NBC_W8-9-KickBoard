7a6c70d9421ad35854e460b27b392296e4ec3bd4
emilyj4482 <emilyj4482@gmail.com>
2024-12-17 14:38:01 +0900

    feat: api key config 파일 적용

--- Quick-Kick/API/APIError.swift
+++ Quick-Kick/API/APIError.swift
@@ -10,3 +10,19 @@ import Foundation
 enum APIError: Error {
     
 }
+
+enum APIKey {
+    static var apiID: String {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY_ID") as? String else {
+            fatalError("API_KEY_ID not found in Info.plist")
+        }
+        return key
+    }
+    
+    static var apiKey: String {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+            fatalError("API_KEY not found in Info.plist")
+        }
+        return key
+    }
+}
