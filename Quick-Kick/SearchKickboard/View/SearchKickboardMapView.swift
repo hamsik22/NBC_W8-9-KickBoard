863e79ee1446563f28f34b7719b1bdf2a6650314
zzangmmz <leemj036@gmail.com>
2024-12-18 03:24:42 +0900

    feat: 모달뷰 UI 테스트용 마커 mapView(viewFor:) 메서드 재정의해서 커스텀, 지도 위에 마커 찍는 addMarker(at:) 메서드 정의, 마커 클릭 이벤트 발생시 mapView(didSelect:) 메서드로 모달뷰 표시

--- Quick-Kick/SearchKickboard/View/SearchKickboardMapView.swift
+++ Quick-Kick/SearchKickboard/View/SearchKickboardMapView.swift
@@ -49,6 +49,9 @@ final class SearchKickboardMapView: MKMapView {
         self.showsUserLocation = true
         self.setUserTrackingMode(.follow, animated: true)
         self.setCameraZoomRange(MKMapView.CameraZoomRange(minCenterCoordinateDistance: 200, maxCenterCoordinateDistance: 2000), animated: true)
+        self.delegate = self
+        
+        self.addMarker(at: CLLocationCoordinate2D(latitude: 37.49467056976841, longitude: 127.02821385513921))
     }
 }
 
@@ -75,5 +78,44 @@ extension SearchKickboardMapView: CLLocationManagerDelegate {
         if let location = userLocation.location {
             setMapCenter(location)
         }
+    }
+}
+
+extension SearchKickboardMapView: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation { return nil }
+        let identifier = "mapPicker"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = false
+        } else {
+            annotationView?.annotation = annotation
+        }
+        annotationView?.image = UIImage(named: "mapPicker")
+        annotationView?.frame.size = CGSize(width: 55, height: 91)
+        return annotationView
+    }
+    
+    func addMarker(at coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        self.addAnnotation(annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
+        if annotation is MKUserLocation {
+            return
+        }
+        
+        let rentKitboardModalView = RentKickboardModalView()
+        self.addSubview(rentKitboardModalView)
+        rentKitboardModalView.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.height.equalTo(180)
+        }
+        rentKitboardModalView.layer.cornerRadius = 40
     }
 }
