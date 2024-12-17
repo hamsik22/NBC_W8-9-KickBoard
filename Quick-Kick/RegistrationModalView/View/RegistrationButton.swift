7d15cee17d00f9215523f8314cfa4402b986235a
Crois0509 <crois0509@naver.com>
2024-12-17 16:41:25 +0900

    add: 모달뷰 등록 버튼 추가

--- Quick-Kick/RegistrationModalView/View/RegistrationButton.swift
+++ Quick-Kick/RegistrationModalView/View/RegistrationButton.swift
@@ -10,4 +10,43 @@ import SnapKit
 
 final class RegistrationButton: UIView {
     
+    private let addButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+        
+        setupButton()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.backgroundColor = .clear
+        
+        setupButton()
+        setupLayout()
+    }
+    
+    private func setupButton() {
+        self.addButton.setTitle("등록하기", for: .normal)
+        self.addButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        self.addButton.backgroundColor = .PersonalNomal.nomal
+        self.addButton.layer.cornerRadius = 30
+        self.addButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        self.addSubview(self.addButton)
+    }
+    
+    @objc private func buttonTapped() {
+        
+    }
+    
+    private func setupLayout() {
+        self.addButton.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
