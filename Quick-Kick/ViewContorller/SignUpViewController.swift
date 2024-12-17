0b45820a710129d08925e3a525887ec2d3edbef6
HwangSukHyeon <payback9488@gmail.com>
2024-12-17 16:53:45 +0900

    file: SignUpViewController.swift

---
+++ Quick-Kick/ViewContorller/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Quick-Kick
+//
+//  Created by 황석현 on 12/16/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
