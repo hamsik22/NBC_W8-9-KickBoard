55a4ae0e702386101e68859e62b6cd6d72c0496e
Crois0509 <crois0509@naver.com>
2024-12-17 16:47:53 +0900

    add: 모달뷰 델리게이트 추가

---
+++ Quick-Kick/AddKickboard/ModalViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ModalViewDelegate.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/17/24.
+//
+
+import UIKit
+
+protocol ModalViewDelegate: AnyObject {
+    
+    func presentModalVIew()
+    
+}
