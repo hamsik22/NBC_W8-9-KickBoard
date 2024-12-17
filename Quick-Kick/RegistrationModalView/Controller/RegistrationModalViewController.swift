47367161703b158b60a48a131e868edced745514
Crois0509 <crois0509@naver.com>
2024-12-17 17:28:06 +0900

    add: 모달뷰 컨트롤러 생성

--- Quick-Kick/RegistrationModalView/Controller/RegistrationModalViewController.swift
+++ Quick-Kick/RegistrationModalView/Controller/RegistrationModalViewController.swift
@@ -10,4 +10,50 @@ import SnapKit
 
 final class RegistrationModalViewController: UIViewController {
     
+    let textField = RegistrationTextField()
+    let typeButton = KickboardTypeButton()
+    let addButton = RegistrationButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupTypeButton()
+        setupTextField()
+        setupAddButton()
+    }
+    
+    private func setupTextField() {
+        view.addSubview(self.textField)
+        
+        self.textField.snp.makeConstraints {
+            $0.top.equalTo(self.typeButton.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(320)
+            $0.height.equalTo(120)
+        }
+    }
+    
+    private func setupTypeButton() {
+        view.addSubview(self.typeButton)
+        
+        self.typeButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(30)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(170)
+            $0.width.equalTo(320)
+        }
+    }
+    
+    private func setupAddButton() {
+        view.addSubview(self.addButton)
+        
+        self.addButton.snp.makeConstraints {
+            $0.top.equalTo(self.textField.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(60)
+            $0.width.equalTo(320)
+        }
+    }
 }
