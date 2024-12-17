7f73c429bdd33195cd460c70b8631ea8afa5b487
Crois0509 <crois0509@naver.com>
2024-12-17 16:17:56 +0900

    add: 모달뷰 버튼 추가

--- Quick-Kick/RegistrationModalView/View/KickboardTypeButton.swift
+++ Quick-Kick/RegistrationModalView/View/KickboardTypeButton.swift
@@ -10,4 +10,97 @@ import SnapKit
 
 final class KickboardTypeButton: UIView {
     
+    private let quickboardNormalTypeButton = UIButton()
+    
+    private let quickboardSeatTypeButton = UIButton()
+    
+    private let normalLabel = UILabel()
+    
+    private let seatLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+        
+        setupButton()
+        setupLabel()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.backgroundColor = .clear
+        
+        setupButton()
+        setupLayout()
+    }
+    
+    private func setupButton() {
+        [self.quickboardNormalTypeButton,
+         self.quickboardSeatTypeButton].forEach {
+            $0.backgroundColor = .clear
+            $0.layer.borderColor = UIColor.PersonalLight.active.cgColor
+            $0.layer.borderWidth = 2
+            $0.layer.cornerRadius = 20
+            $0.imageView?.contentMode = .scaleAspectFit
+            $0.contentEdgeInsets = .init(top: 10, left: 10, bottom: 10, right: 10)
+            self.addSubview($0)
+        }
+        
+        setupImage()
+    }
+    
+    private func setupImage() {
+        self.quickboardNormalTypeButton.setImage(UIImage(named: "QuickBoard"), for: .normal)
+        self.quickboardSeatTypeButton.setImage(UIImage(named: "QuickBoard - Seat"), for: .normal)
+    }
+    
+    private func setupAction() {
+        self.quickboardNormalTypeButton.addTarget(self, action: #selector(selectedNormalButton), for: .touchUpInside)
+        self.quickboardSeatTypeButton.addTarget(self, action: #selector(selectedSeatButton), for: .touchUpInside)
+    }
+    
+    @objc private func selectedNormalButton() {
+        
+    }
+    
+    @objc private func selectedSeatButton() {
+        
+    }
+    
+    private func setupLabel() {
+        [self.normalLabel, self.seatLabel].forEach {
+            $0.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+            $0.textColor = .black
+            $0.textAlignment = .center
+            self.addSubview($0)
+        }
+        
+        self.normalLabel.text = "일반형"
+        self.seatLabel.text = "안장형"
+    }
+    
+    private func setupLayout() {
+        self.quickboardNormalTypeButton.snp.makeConstraints {
+            $0.leading.top.equalToSuperview()
+            $0.height.width.equalTo(150)
+        }
+        
+        self.quickboardSeatTypeButton.snp.makeConstraints {
+            $0.trailing.top.equalToSuperview()
+            $0.height.width.equalTo(150)
+        }
+        
+        self.normalLabel.snp.makeConstraints {
+            $0.centerX.equalTo(self.quickboardNormalTypeButton)
+            $0.top.equalTo(self.quickboardNormalTypeButton.snp.bottom).offset(5)
+        }
+        
+        self.seatLabel.snp.makeConstraints {
+            $0.centerX.equalTo(self.quickboardSeatTypeButton)
+            $0.top.equalTo(self.quickboardSeatTypeButton.snp.bottom).offset(5)
+        }
+    }
 }
