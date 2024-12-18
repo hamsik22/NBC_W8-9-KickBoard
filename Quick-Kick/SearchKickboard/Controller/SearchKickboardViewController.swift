0c643dfa73d6c83a253c4bd965f646e7bf7ed6bd
zzangmmz <leemj036@gmail.com>
2024-12-18 20:47:22 +0900

    add: 목데이터 코드 추가

--- Quick-Kick/SearchKickboard/Controller/SearchKickboardViewController.swift
+++ Quick-Kick/SearchKickboard/Controller/SearchKickboardViewController.swift
@@ -7,10 +7,54 @@
 import UIKit
 
 final class SearchKickboardViewController: UIViewController {
-    private let kickboards: [Kickboard] = CoreDataManager.shared.fetchKickboards()
-    
+    let searchKickboardView = SearchKickboardView()
     override func loadView() {
         super.loadView()
-        view = SearchKickboardView()
+        view = searchKickboardView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchKickboardView.delegate = self
+        makeMockData()
+    }
+}
+
+extension SearchKickboardViewController: SearchKickboardViewDelegate {
+    func getKickboardsData() -> [Kickboard] {
+        return CoreDataManager.shared.fetchKickboards()
+    }
+    
+    private func makeMockData() {
+        CoreDataManager.shared.createKickboard(
+            nickName: "씽씽이A",
+            isSaddled: false,
+            isOccupied: false,
+            startTime: nil,
+            endTime: nil,
+            latitude: 37.498095,
+            longitude: 127.027610,
+            address: "강남역 1번출구"
+        )
+        CoreDataManager.shared.createKickboard(
+            nickName: "씽씽이B",
+            isSaddled: true,
+            isOccupied: true,
+            startTime: Date(),
+            endTime: Date().addingTimeInterval(3600),
+            latitude: 37.497922,
+            longitude: 127.027637,
+            address: "강남역 2번출구"
+        )
+        CoreDataManager.shared.createKickboard(
+            nickName: "씽씽이C",
+            isSaddled: false,
+            isOccupied: false,
+            startTime: nil,
+            endTime: nil,
+            latitude: 37.504507,
+            longitude: 127.048962,
+            address: "삼성중앙역 4번출구"
+        )
     }
 }
