50fa8533d7efbadee625a26c34339e552ff62b04
Crois0509 <crois0509@naver.com>
2024-12-19 00:23:28 +0900

    file: 온보딩 페이지 파일 생성

---
+++ Quick-Kick/OnboadingPage/OnboadingView/OnboadingPageView.swift
@@ -0,0 +1,13 @@
+//
+//  OnboadingPageView.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/19/24.
+//
+
+import UIKit
+import SnapKit
+
+final class OnboadingPageView: UIView {
+    
+}
---
+++ Quick-Kick/OnboadingPage/OnboadingViewController/OnboadingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnboadingViewController.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/19/24.
+//
+
+import UIKit
+
+final class OnboadingViewController: UIViewController {
+    
+}
