a0a294d9614c9b825c58571f9b692423d8efe7a5
Crois0509 <crois0509@naver.com>
2024-12-19 14:18:13 +0900

    add: 온보딩 델리게이트 생성

---
+++ Quick-Kick/OnboadingPage/OnboardingDataModel/OndoardingDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  OndoardingDelegate.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/19/24.
+//
+
+import UIKit
+
+protocol OndoardingDelegate: AnyObject {
+    
+    func changedView()
+    
+}
