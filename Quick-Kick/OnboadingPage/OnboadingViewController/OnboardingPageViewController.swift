ca3bdc2c827dfd5b6e4ab9c4bc015272ff4220d2
Crois0509 <crois0509@naver.com>
2024-12-19 15:13:59 +0900

    feat: 온보딩뷰 완전 구현

--- Quick-Kick/OnboadingPage/OnboadingViewController/OnboardingPageViewController.swift
+++ Quick-Kick/OnboadingPage/OnboadingViewController/OnboardingPageViewController.swift
@@ -10,22 +10,27 @@ import SnapKit
 
 final class OnboardingPageViewController: UIViewController {
     
-    private let firstOnboardingView = OnboardingDataModel(title: "환영합니다!!",
-                                                         info: "퀵킥은 목적지까지\n빠르게 이동하기 위해\n공용 킥보드를\nQuick 하게\n빌릴 수 있는 앱입니다!",
-                                                         image: UIImage(named: "onboadingImage_kickboard"),
-                                                          isButtonHidden: true)
+    private let models: [OnboardingDataModel] = {
+        
+        let firstOnboardingView = OnboardingDataModel(title: "환영합니다!!",
+                                                      info: "퀵킥은 목적지까지\n빠르게 이동하기 위해\n공용 킥보드를\nQuick 하게\n빌릴 수 있는 앱입니다!",
+                                                      image: UIImage(named: "onboadingImage_kickboard"),
+                                                      isButtonHidden: true)
+        
+        let secondOnboardingView = OnboardingDataModel(title: "잠깐!!",
+                                                       info: "안전모와 면허는\n필수인거 아시죠??",
+                                                       image: UIImage(named: "onboadingImage_safe"),
+                                                       isButtonHidden: true)
+        
+        let thirdOnboardingView = OnboardingDataModel(title: "시작합시다!!",
+                                                      info: "퀵킥에 로그인하고\nQuick하게\n킥보드를 이용해요!!",
+                                                      image: nil,
+                                                      isButtonHidden: false)
+        
+        return [firstOnboardingView, secondOnboardingView, thirdOnboardingView]
+    }()
     
-    private let secondOnboardingView = OnboardingDataModel(title: "잠깐!!",
-                                                          info: "안전모와 면허는\n필수인거 아시죠??",
-                                                          image: UIImage(named: "onboadingImage_safe"),
-                                                           isButtonHidden: true)
-    
-    private let thirdOnboardingView = OnboardingDataModel(title: "시작합시다!!",
-                                                         info: "퀵킥에 로그인하고\nQuick하게\n킥보드를 이용해요!!",
-                                                         image: nil,
-                                                          isButtonHidden: false)
-    
-    private lazy var onboardingView: OnboardingPageView = .init(data: self.firstOnboardingView)
+    private lazy var onboardingView: OnboardingPageView = .init(data: self.models[self.currentIndex])
     
     private var pageControl = UIPageControl()
         
@@ -38,6 +43,7 @@ final class OnboardingPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.onboardingView.onboardingDelegate = self
         setupPageControl()
         setupLayout()
     }
@@ -55,6 +61,33 @@ final class OnboardingPageViewController: UIViewController {
         self.pageControl.snp.makeConstraints {
             $0.trailing.leading.equalToSuperview()
             $0.bottom.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func swipePage() {
+        UIView.transition(with: self.onboardingView, duration: 0.5, options: .transitionCrossDissolve) {
+            self.setupPageControl()
+            self.onboardingView.changedData(data: self.models[self.currentIndex])
+        }
+    }
+}
+
+extension OnboardingPageViewController: OndoardingDelegate {
+    func changedView(_ direction: UISwipeGestureRecognizer.Direction) {
+        switch direction {
+        case .right:
+            if self.currentIndex > 0 {
+                self.currentIndex -= 1
+                swipePage()
+            }
+            
+        case .left:
+            if self.currentIndex < self.models.count - 1 {
+                self.currentIndex += 1
+                swipePage()
+            }
+            
+        default: break
         }
     }
 }
