abe2d8b3e3223890c743de356a3ab725e91ef5f2
zzangmmz <leemj036@gmail.com>
2024-12-19 23:51:22 +0900

    fix: 누락된 editKickboardModalView() 메서드 추가

--- Quick-Kick/AddKickboard/AddKickboardViewController.swift
+++ Quick-Kick/AddKickboard/AddKickboardViewController.swift
@@ -25,6 +25,17 @@ class AddKickboardViewController: UIViewController {
 }
 
 extension AddKickboardViewController: MapViewDelegate, ModalViewDelegate {
+    func editKickboardModalView() {
+        let modalVC = RegistrationModalViewController()
+        modalVC.editKickboardData(false, "Sparta의 킥보드") // 추후 코어데이터에서 데이터를 받아와서 입력
+        
+        modalVC.modalPresentationStyle = .formSheet
+        modalVC.sheetPresentationController?.preferredCornerRadius = 50
+        modalVC.sheetPresentationController?.detents = [.medium()]
+        
+        self.present(modalVC, animated: true)
+    }
+    
     func requestNaverAddress(lat: String, lon: String) {
         addressRepository.fetchAddress(lat: lat, lon: lon)
     }
