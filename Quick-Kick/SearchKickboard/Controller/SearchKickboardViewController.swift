8011c198fe0b750f393f0418da8e59116bd9969b
zzangmmz <leemj036@gmail.com>
2024-12-19 23:55:18 +0900

    refactor: 디버그용 코드 삭제

--- Quick-Kick/SearchKickboard/Controller/SearchKickboardViewController.swift
+++ Quick-Kick/SearchKickboard/Controller/SearchKickboardViewController.swift
@@ -19,23 +19,5 @@ final class SearchKickboardViewController: UIViewController {
     private func fetchKickboards() {
         let kickboards = CoreDataManager.shared.fetch()
         searchKickboardView.deliverKickboardsData(kickboards)
-        makeMockData()
-    }
-}
-
-extension SearchKickboardViewController {
-    private func makeMockData() {
-        print(CoreDataManager.shared.fetch())
-        let data = KickboardDTO(
-            nickName: "씽씽이A",
-            isSaddled: false,
-            isOccupied: false,
-            startTime: nil,
-            endTime: nil,
-            latitude: 37.517430,
-            longitude: 127.047515,
-            address: "강남역 1번출구"
-        )
-        CoreDataManager.shared.create(with: data)
     }
 }
