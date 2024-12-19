d80a95ead73610db2543bd96072bd29c50b093a2
Crois0509 <crois0509@naver.com>
2024-12-19 14:18:03 +0900

    add: 온보딩 데이터 모델 생성

---
+++ Quick-Kick/OnboadingPage/OnboardingDataModel/OnboardingDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingDataModel.swift
+//  Quick-Kick
+//
+//  Created by 장상경 on 12/19/24.
+//
+
+import UIKit
+
+struct OnboardingDataModel {
+    let title: String
+    let info: String
+    let image: UIImage?
+    let isButtonHidden: Bool
+}
